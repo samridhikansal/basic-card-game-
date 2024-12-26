246a2f84145277afb3aa114924263313b494f56a
samridhikansal <kansal.samridhi@gmail.com>
2024-12-26 16:09:17 -0700

    Card has a value and a suit type

--- card.py
+++ card.py
@@ -1,18 +1,17 @@
+from enum import Enum
+
 class Card:
   
-  type = ["diamond", "heart", "spade", "club"]
-  color = ["red", "black"]
-  number = range(1,14)
-  def __init__(self, color, number, type):
-    self.color = color
-    self.number = number
-    self.type = type
-  def __str__(self):
-    cardType = str(self.type)
-    cardColor = str(self.color)
-    cardNumber = str(self.number)
-    result = cardType + " " + cardColor + " " + cardNumber
-    return(result)
-  
+  def __init__(self, suit, value):
+    self.suit= suit
+    self.value= value
+
+class cardDeck(Enum):
+  SPADE = "spade"
+  DIAMOND = "diamond"
+  HEART = "heart"
+  CLUB = "club"
+
+  print(SPADE)
   
 # This class defines a simple class of cards. Each card has a color, a number and a type. 
